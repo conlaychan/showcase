e00749f9a101523faa21582646e3cc9ac63fcd3e
陈增辉 <conlay@qq.com>
2017-08-30 15:07:39 +0800

    init

--- showcase-service/src/main/java/com/logibeat/cloud/showcase/dao/UserDao.java
+++ showcase-service/src/main/java/com/logibeat/cloud/showcase/dao/UserDao.java
@@ -1,10 +1,11 @@
 package com.logibeat.cloud.showcase.dao;
 
 import com.logibeat.cloud.boot.mybatis.MybatisDao;
+import com.logibeat.cloud.showcase.criteria.UserCriteria;
 import com.logibeat.cloud.showcase.entity.User;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class UserDao extends MybatisDao<User> {
+public class UserDao extends MybatisDao<User, UserCriteria> {
 
 }
