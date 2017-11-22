718f631e861de69b02e5d6c888b8e27f8f013f91
陈增辉 <conlay@qq.com>
2017-11-22 16:01:06 +0800

    设置日志实现框架

--- showcase-web/src/main/java/com/logibeat/cloud/showcase/web/ShowcaseWebApplication.java
+++ showcase-web/src/main/java/com/logibeat/cloud/showcase/web/ShowcaseWebApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShowcaseWebApplication {
     public static void main(String[] args) {
+        // 设置日志实现框架
+        // System.setProperty("org.springframework.boot.logging.LoggingSystem", "org.springframework.boot.logging.logback.LogbackLoggingSystem");
         SpringApplication.run(ShowcaseWebApplication.class, args);
     }
 }
